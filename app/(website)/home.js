e65175578d58eacb4e4366239243fa85d744162a
Carlo Kruger <ironicbuddha@gmail.com>
2023-12-24 15:35:03 +0200

    made the background image full width

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -1,15 +1,28 @@
+import React from "react";
 import Link from "next/link";
 import Container from "@/components/container";
 import PostList from "@/components/postlist";
 
-export default function Post({ posts }) {
+const Home = () => {
+  const backgroundImage = '/img/background.jpeg';
+
   return (
-    <>
-      {posts && (
-        <Container className="container">
-          <img src="/img/background.jpeg" alt="Background" />
-        </Container>
+    <Container>
+      {backgroundImage && (
+        <div
+          className="custom-bg-image ml-0"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
+          <div className="text-white text-center pt-32">
+            <h1 className="text-4xl font-bold mb-4">carlo kruger</h1>
+            <p className="text-lg">
+              this is my space
+            </p>
+          </div>
+        </div>
       )}
-    </>
+    </Container>
   );
-}
+};
+
+export default Home;
