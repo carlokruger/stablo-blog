f0abcf8290693dad17ca141f4ad0a58878717d0a
Carlo Kruger <ironicbuddha@gmail.com>
2023-12-29 11:05:52 +0200

    updated label colour

--- components/ui/label.js
+++ components/ui/label.js
@@ -23,7 +23,7 @@ export default function Label(props) {
     return (
       <div
         className={
-          "inline-flex items-center justify-center font-bold px-2 h-6 text-sm bg-gray-50 text-gray-500 rounded-full shrink-0 dark:bg-gray-800 dark:text-gray-300"
+          "inline-flex items-center justify-center font-bold px-2 h-6 text-sm bg-blue-50 text-blue-500 rounded-full shrink-0 dark:bg-gray-800 dark:text-gray-300"
         }>
         {props.children}
       </div>
@@ -35,7 +35,7 @@ export default function Label(props) {
       className={cx(
         "inline-block text-xs font-medium tracking-wider lowercase ",
         !margin && " mt-5",
-        color[props.color] || color[gray]
+        color[props.color] || color["grey"]
       )}>
       {props.children}
     </span>
