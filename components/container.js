d9a17c265791c8fbec07dfd47380004973dc5422
carlokruger <ironicbuddha@gmail.com>
2024-11-04 13:59:48 +0200

    Update container.js

--- components/container.js
+++ components/container.js
@@ -4,11 +4,10 @@ export default function Container(props) {
   return (
     <div
       className={cx(
-        "bg-container px-8 mx-auto xl:px-5",
+        "container mx-auto px-8 ",
         props.large ? " max-w-screen-xl" : " max-w-screen-lg",
         !props.alt && "py-5 lg:py-8",
-        props.className,
-        "w-screen"
+        props.className
       )}>
       {props.children}
     </div>
